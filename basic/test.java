bb54b34aa07652ce42a0a605dac39158232b1bc5
boheningmengcao <mingshuo.li.cen@gmail.com>
2016-09-13 00:40:54 -0400

    Update test.java

--- basic/test.java
+++ basic/test.java
@@ -1,3 +1,11 @@
-public void binarys(int[] a){
-   return 0;
+public static int binarySearch(int[] a, int key) {
+int lo = 0;
+int hi = a.length - 1;
+while (lo <= hi) {
+int mid = lo + (hi - lo) / 2;
+if (key < a[mid]) hi = mid - 1;
+else if (key > a[mid]) lo = mid + 1;
+else return mid;
+}
+return -1;
 }
