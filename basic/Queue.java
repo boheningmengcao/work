c95fc2d73372c92b774180f414fcdac2936691c5
boheningmengcao <mingshuo.li.cen@gmail.com>
2016-09-15 18:24:04 -0400

    Update Queue.java

--- basic/Queue.java
+++ basic/Queue.java
@@ -1,1 +1,20 @@
-
+class Animal{
+  int order;
+  String name;
+}
+class Dog extends Animal[]
+class Cat extends Animal[]
+Animal clequeueAny(){
+  Queue<Dog> dogs=new LinkedList<Dog>();
+  Queue<Cat> cats=new LinkedList<Cat>();
+  Animal dog=dogs.element();
+  Animal cat=cats.element();
+  if(dog.order<cat.order){
+    dogs.remove();
+    return dog;
+  }
+  else{
+    cats.remove();
+    return cat;
+  }
+}
