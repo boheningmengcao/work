998a07c74a5e776f73c304760c52641e5317ede6
boheningmengcao <mingshuo.li.cen@gmail.com>
2016-09-15 00:07:32 -0400

    Update ReverseArray.java

--- basic/ReverseArray.java
+++ basic/ReverseArray.java
@@ -1,3 +1,5 @@
+method 1
+
 void reverse(int[] a){  
   for(int i=0;i<a.length/2;i++){
     int temp=a[i];
@@ -5,4 +7,14 @@ void reverse(int[] a){
     a[a.length-1-i];
   }
 }  
+
+method 2
+
+void reverse(int[]a){
+  for(int i=0;j<a.length-1;i<j;i++,j--){
+    int temp=a[i];
+    a[i]=a[j];
+    a[j]=temp;
+  }
+}
   
