ac6daae692e4530d37192d2ec30593f4cf1db021
boheningmengcao <mingshuo.li.cen@gmail.com>
2016-09-15 18:12:42 -0400

    Update Stack.java

--- basic/Stack.java
+++ basic/Stack.java
@@ -1,1 +1,11 @@
-Stack 
+Stack<Integer> Sort(Stack<Integer> S){
+  Stack<Integer> S2=new Stack<Integer>();
+  while(!S.isEmpty()){
+    Int top=s.pop;
+    while(!S2.isEmpty()&&S2.peek()>top){
+      S.push(S2.pop());
+    }
+    S2.push(top);
+  }
+  return S2;
+}
