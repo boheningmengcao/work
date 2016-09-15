4850204efb253b90157448994751d2bb3bb07194
boheningmengcao <mingshuo.li.cen@gmail.com>
2016-09-15 17:32:48 -0400

    Update LinkedList.java

--- basic/LinkedList.java
+++ basic/LinkedList.java
@@ -1,3 +1,23 @@
 1
 
-Node newNode=new Node()；
+int count(Node head){
+  int i=0;Node n=head;
+  While(n!==null){
+    n=n.nest;
+    i++
+  }
+  return i;
+}
+
+2.
+int sum(Node head){
+  int s=0;Node n=head;
+  while(n!==null){
+    s+=n.val;
+    n=n.next;
+  }
+  return s;
+}
+
+
+
